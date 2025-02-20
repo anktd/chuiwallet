1a640865f95aa78850a54e885ab8dfae92f1529d
mtfuji25 <58710601+mtfuji25@users.noreply.github.com>
2025-02-21 03:15:10 +0900

    fee estimator with coingecko api integration

---
+++ packages/backend/src/walletManager.ts
@@ -0,0 +1,20 @@
+import type { WalletOptions } from './modules/wallet.js';
+import Wallet from './modules/wallet.js';
+
+export default class WalletManager {
+  private wallets: Wallet[];
+
+  constructor() {
+    this.wallets = [];
+  }
+
+  public createWallet(options: WalletOptions): Wallet {
+    const wallet = new Wallet(options);
+    this.wallets.push(wallet);
+    return wallet;
+  }
+
+  public getWallet(index: number = 0): Wallet {
+    return this.wallets[index];
+  }
+}
