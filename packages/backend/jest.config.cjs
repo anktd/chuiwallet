f725d7715713d16981c6a39a48e44964cf2f0f10
mtfuji25 <58710601+mtfuji25@users.noreply.github.com>
2025-02-26 14:39:20 +0900

    fix jest config

--- packages/backend/jest.config.cjs
+++ packages/backend/jest.config.cjs
@@ -1,16 +1,20 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest/presets/default-esm',
+  preset: 'ts-jest/presets/default-esm', // Use the ESM preset
   testEnvironment: 'node',
-  extensionsToTreatAsEsm: ['.ts'],
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/*.test.ts'],
   globals: {
     'ts-jest': {
       useESM: true,
     },
   },
-  moduleNameMapper: {
-    '^@samouraiwallet/electrum-client$': '<rootDir>/__mocks__/@samouraiwallet/electrum-client.js',
-    '^(\\.{1,2}/.*)\\.js$': '$1',
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
   },
-  transform: {},
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+    '^@samouraiwallet/electrum-client$': '<rootDir>/__mocks__/@samouraiwallet/electrum-client.js',
+  },
+  transformIgnorePatterns: ['/node_modules/'],
 };
