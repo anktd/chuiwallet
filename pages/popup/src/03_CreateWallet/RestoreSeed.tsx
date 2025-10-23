dfb156512d7a92d873fc8213f8f0d737d74e4637
Vin Lim <vin.lim.yh@gmail.com>
2025-10-23 14:03:22 +0800

    Add back button for seed phrase

--- pages/popup/src/03_CreateWallet/RestoreSeed.tsx
+++ pages/popup/src/03_CreateWallet/RestoreSeed.tsx
@@ -89,8 +89,22 @@ export const RestoreSeed: React.FC = () => {
 
   return (
     <div
-      className="relative flex overflow-hidden flex-col px-5 pt-12 pb-[19px] bg-dark h-full w-full"
+      className="relative flex overflow-hidden flex-col px-5 px-4pb-[19px] bg-dark h-full w-full"
       onPaste={handleContainerPaste}>
+      <div className="flex gap-10 justify-between items-center self-stretch py-3 w-full text-xs font-bold leading-6 bg-dark min-h-[48px] text-neutral-200">
+        <button onClick={() => navigate('/onboard/choose-method')} className="flex justify-start items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+          </svg>
+          Back
+        </button>
+      </div>
       <div className="flex flex-col self-center w-full text-center">
         <div className="flex flex-col w-full">
           <div className="text-2xl font-bold leading-loose text-white">Input your seed phrase</div>
